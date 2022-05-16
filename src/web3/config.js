91980b49d8b0df298b3bac372aa081e4e2b5786a
Raoooooo <ralinhao@gmail.com>
2022-05-16 18:20:23 +0800

    init project

---
+++ src/web3/config.js
@@ -0,0 +1,44 @@
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import Torus from '@toruslabs/torus-embed';
+import Authereum from 'authereum';
+
+import { Bitski } from 'bitski';
+
+import Fortmatic from 'fortmatic';
+// eslint-disable-next-line global-require
+const { USDT_ADDRESS } = process.env.NODE_ENV === 'production' ? require('./constants') : require('./constants.dev');
+//
+const providerOptions = {
+  // https://docs.walletconnect.org/
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: '7581b5aab9b4489ba1517a3e06e84280', // TODO infuraId
+    },
+  },
+  // https://github.com/torusresearch/torus-embed#readme
+  torus: {
+    package: Torus,
+  },
+  // https://docs.fortmatic.com/
+  fortmatic: {
+    package: Fortmatic,
+    options: {
+      key: 'http://localhost', // TODO key
+    },
+  },
+  // https://docs.authereum.com/integration
+  authereum: {
+    package: Authereum,
+  },
+  // https://docs.bitski.com/
+  bitski: {
+    package: Bitski,
+    options: {
+      clientId: 'xxxxxxxxxx', // TODO
+      callbackUrl: `${window.location.href}bitski-callback.html`,
+    },
+  },
+};
+
+export { USDT_ADDRESS, providerOptions };
